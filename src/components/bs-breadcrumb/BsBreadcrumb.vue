8e8e375eefdcef87c9baf0b5fa298b493c35a00a
Gabor Zemko <gaborzemko@gmail.com>
2021-09-01 23:49:54 +0200

    feat: Breadcrumb component.

---
+++ src/components/bs-breadcrumb/BsBreadcrumb.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li
+        v-for="(item, index) in items"
+        :key="index"
+        class="breadcrumb-item"
+        :class="{ active: isActiveItem(item)}"
+      >
+        <router-link
+          v-if="!isActiveItem(item)"
+          :to="item.route"
+          v-text="item.title"
+        />
+        <span v-else v-text="item.title" />
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'BsBreadcrumb',
+  props: {
+    /**
+     * List of breadcrumb items.
+     */
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    /**
+     * Is active item
+     *
+     * @param item
+     * @returns {boolean}
+     */
+    isActiveItem(item) {
+      return item.route.name === this.$route.name
+    },
+  },
+}
+</script>
