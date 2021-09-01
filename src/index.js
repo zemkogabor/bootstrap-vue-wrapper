24f479553d9ddb597000ea9d88b4cfff48dd4bcb
Gabor Zemko <gaborzemko@gmail.com>
2021-09-02 00:51:08 +0200

    fix: Components exports.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import BsBreadcrumb from './components/bs-breadcrumb/BsBreadcrumb.vue'
+import BsForm from './components/bs-form/BsForm.vue'
+import BsInput from './components/bs-input/BsInput.vue'
+import BsPaginator from './components/bs-paginator/BsPaginator.vue'
+import BsTable from './components/bs-table/BsTable.vue'
+import BsToast from './components/bs-toast/BsToast.vue'
+import Validator from './mixins/validator.js'
 
 export {
-    BsBreadcrumb
+  BsBreadcrumb,
+  BsForm,
+  BsInput,
+  BsPaginator,
+  BsTable,
+  BsToast,
+  Validator,
 }
