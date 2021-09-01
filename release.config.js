c76f63f80d5f4bdca02f333b986cc720bb7ef05c
Gabor Zemko <gaborzemko@gmail.com>
2021-09-01 22:18:27 +0200

    Release config.

---
+++ release.config.js
@@ -0,0 +1,15 @@
+export default {
+    release: {
+        branch: 'main',
+    },
+    plugins: [
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json'],
+                message: '${nextRelease.version}',
+            },
+        ],
+    ],
+}
