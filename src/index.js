f39872f86087771d7333f37df71f4a933e24059b
Gabor Zemko <gaborzemko@gmail.com>
2021-09-22 20:13:37 +0200

    Textarea added.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import BsBreadcrumb from './components/bs-breadcrumb/BsBreadcrumb.vue'
 import BsForm from './components/bs-form/BsForm.vue'
 import BsInput from './components/bs-input/BsInput.vue'
+import BsTextarea from './components/bs-textarea/BsTextarea.vue'
 import BsPaginator from './components/bs-paginator/BsPaginator.vue'
 import BsTable from './components/bs-table/BsTable.vue'
 import BsToast from './components/bs-toast/BsToast.vue'
@@ -10,6 +11,7 @@ export {
   BsBreadcrumb,
   BsForm,
   BsInput,
+  BsTextarea,
   BsPaginator,
   BsTable,
   BsToast,
