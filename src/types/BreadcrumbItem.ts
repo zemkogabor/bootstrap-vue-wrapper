5885bc46c7d83b817019c51fa55e9e46c6202861
Gabor Zemko <gaborzemko@gmail.com>
2025-12-01 16:00:31 +0100

    breacrumb fix

--- src/types/BreadcrumbItem.ts
+++ src/types/BreadcrumbItem.ts
@@ -1,7 +1,7 @@
 export default interface BreadcrumbItem {
   route?: {
     name: string
-    query?: string
+    query?: Record<string, string | number>
     params?: Record<string, string | number>
   },
   title: string;
